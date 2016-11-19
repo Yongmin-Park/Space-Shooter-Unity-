c6403c2218c164d50ee81894ea448b4a570f5bdb
Yongmin Park <yongmin.txt@gmail.com>
2016-11-19 14:11:03 +0000

    Add files via upload

---
+++ RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	float tumble = 5;
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+
+}
---
+++ WeaponController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+	public GameObject shot;
+	public Transform firePosition;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating ("Fire", 1, 2);
+	}
+
+	void Fire () 
+	{
+		Instantiate (shot, firePosition.position, firePosition.rotation);
+	}
+}
